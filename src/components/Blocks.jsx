0d6ec6cbd380a292ba50bb887d57d8a299971512
Tatiana-Juin <username@users.noreply.github.com>
2026-06-16 23:10:44 +0200

    J'ai ajouter les fleche et la corbeille dans les card des blocks

--- src/components/Blocks.jsx
+++ src/components/Blocks.jsx
@@ -4,6 +4,12 @@ export default function Blocks({text}) {
   return (
     <div className='oneBlock'>
         <p>{text}</p>
+        <div className="block-actions">
+           <button className="btn-icon" title="Monter">↑</button>
+           <button className="btn-icon" title="Descendre">↓</button>
+           
+           <button className="btn-icon delete" title="Supprimer">🗑</button> 
+        </div>
     </div>
   )
 }
