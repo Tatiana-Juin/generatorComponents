04d33dbc196ab9eca5565ad033c168ede8443706
Tatiana-Juin <username@users.noreply.github.com>
2026-06-23 23:45:39 +0200

    J'ai fait le design je doit encore faire quelque amelioration pour générer le code

--- src/features/codegen/CodeGenerator.jsx
+++ src/features/codegen/CodeGenerator.jsx
@@ -24,22 +24,33 @@ function generateCode(block) {
       return '';
   }
 }
+
+
+
 export default function CodeGenerator({block,onGenerate}) {
+  const generatedCode = block.map(b => generateCode(b)).join("\n");
+
+  // fonction pour copier 
+  function handleCopy(){
+    navigator.clipboard.writeText(generatedCode);
+  
+  }
   return (
   
     <div className="popup-overlay">
       <div className="popup-content">
         
         <div className="popup-header">
-          <h2>Code Généré</h2>
+          <h2>Code Généré - HTML CSS</h2>
           {/* Un petit bouton pour fermer la pop-up */}
           <button className="close-btn" onClick={onGenerate}>✕</button>
         </div>
 
         <div className="popup-body">
+          <button className="preview-button" onClick={handleCopy}> Copier</button>
           <pre className="code-box">
            <code>
-              {block.map(b => generateCode(b)).join("\n")}
+              {generatedCode}
             </code>
           </pre>
         </div>
